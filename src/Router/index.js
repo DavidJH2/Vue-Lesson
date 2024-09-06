876e512a3cd4e36b90d2f30e576581976ed5ff47
David <djhatvr@gmail.com>
2024-09-05 17:19:18 -0700

    Commit Project

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,6 +3,8 @@ import HomeView from "./Views/HomeView.vue";
 import JobsView from "./Views/JobsView.vue";
 import NotFound from "./Views/NotFound.vue";
 import JobView from "./Views/JobView.vue";
+import AddJobView from "./Views/AddJobView.vue";
+import EditJob from "./Views/EditJob.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,16 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job',
             component: JobView,
+        },
+        {
+            path: '/jobs/Add',
+            name: 'add-job',
+            component: AddJobView,
+        },
+        {
+            path: '/Jobs/Edit/:id',
+            name: 'edit-job',
+            component: EditJob,
         },
         {
             path: '/:catchAll(.*)',
